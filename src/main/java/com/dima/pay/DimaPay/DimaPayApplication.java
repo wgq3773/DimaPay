d750a766aca9347e70efe68972ea7f476251c8ec
wgq-t245 <wgq-t245@DLT01164.dinpay.net>
2018-11-29 09:39:12 +0800

    delete @EnableWebMvc

--- src/main/java/com/dima/pay/DimaPay/DimaPayApplication.java
+++ src/main/java/com/dima/pay/DimaPay/DimaPayApplication.java
@@ -5,9 +5,7 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableWebMvc
 @SpringBootApplication
 @ImportResource("classpath:applicationContext-*.xml")
 public class DimaPayApplication {
