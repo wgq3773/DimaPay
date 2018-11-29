0890e845fd7f251e35d3414b2aef4aa90713a129
wgq-t245 <wgq-t245@DLT01164.dinpay.net>
2018-11-29 18:14:02 +0800

    update pay notify return

---
+++ src/main/java/com/dima/pay/DimaPay/notify/ReturnController.java
@@ -0,0 +1,22 @@
+package com.dima.pay.DimaPay.notify;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.dima.commons.constant.RequestUrl;
+
+@Controller
+public class ReturnController {
+
+	/**
+	 * 同步通知
+	 * @return
+	 */
+	@RequestMapping(value = RequestUrl.RETURN_URL)
+	public ModelAndView receiveRetrun(){
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("forward:/contentList");
+		return mv;
+	}
+}
